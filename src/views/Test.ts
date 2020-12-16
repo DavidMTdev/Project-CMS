b90d2b1e31309364d84aa25bda7ed1ad43d56fca
loux22 <louis.ardilly@yahoo.fr>
2020-12-16 15:16:53 +0100

    add style test

--- src/views/Test.ts
+++ src/views/Test.ts
@@ -5,4 +5,23 @@ import '@/assets/sass/test.scss';
 
 @WithRender
 @Component
-export default class Test extends Vue {}
+export default class Test extends Vue {
+    // component: [
+    //     {
+    //       id: 0,
+    //       title: 'Item A',
+    //       list: 1
+    //     },
+    //     {
+    //       id: 1,
+    //       title: 'Item B',
+    //       list: 1
+    //     },
+    //     {
+    //       id: 2,
+    //       title: 'Item C',
+    //       list: 2
+    //     }]
+
+    //     constructor(component) { 
+}
