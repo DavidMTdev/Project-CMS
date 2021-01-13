f48c477e9608ed6b367ea1375227094d77e9e84e
Leo Peyre <leo.peyre95@gmail.com>
2021-01-13 17:47:09 +0100

    Juste regler la taille des images de la nav et c'est bon bisous

--- src/views/Home.ts
+++ src/views/Home.ts
@@ -1,15 +1,21 @@
 import { Component, Vue } from 'vue-property-decorator';
 import WithRender from '@/views/templates/home.html'
+import MenuProjet from '@/components/MenuProjet'
+
 
 @WithRender
-@Component
+@Component({
+    components: {
+      MenuProjet
+  }
+})
 export default class Home extends Vue {
     
     message = 'hello world'
 
-    setMessage(message: string): void {
-        this.message = message
-    }
+    // setMessage(message: string): void {
+    //     this.message = message
+    // }
 
 
 }
