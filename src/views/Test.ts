f7e5a6741e043598eba35887d212a9100b7d4d72
DavidMTdev <david.teixeira31@gmail.com>
2021-03-03 18:43:42 +0100

    Add systeme editable

--- src/views/Test.ts
+++ src/views/Test.ts
@@ -4,26 +4,27 @@ import WithRender from '@/templates/test.html'
 import '@/assets/sass/test.scss';
 
 import Dom from './Dom'
-import ComponentEdit from '@/utils/Component';
+import DragAndDrop from '@/utils/DragAndDrop';
 
 @WithRender
 @Component
 export default class Test extends Vue {
-    compo: Array<ComponentEdit> = [];
-    element!: ComponentEdit;
+    compo: Array<DragAndDrop> = [];
+    element!: DragAndDrop;
     count!: number;
 
     mounted() {
-        this.compo.push(new ComponentEdit("h1", null, null, "mon super titre"));  
-        this.compo.push(new ComponentEdit("div", "section", "container"));  
-        this.compo.push(new ComponentEdit("p", null, "text", "Hanc Atratino si cum qui ignosco acriter Atratino excusationem vel odio si meo est habet spei ignosco nihil optimo liceret hac excusationem habet libidine volueritis causa resistendum nec intolerabili cui si Si Ceteris nihil non necessitati de nisi spei nimis non existimare ego descensurum accusationem voluit excusationem spei meo descensurum volueritis vel ego vel ignosco causa intolerabili accusare optimo excusationem nec non causa speravit acerbo modo niteretur niteretur aetatis descendisset volueritis constituetis nimis fuisse necessario est alicuius sic descensurum necessario nec necessario iussus descensurum ignosco descendisset optimo aliquid iudices iussus modo si iudices Si necessitati est pietatis iudices quicquam etiam."));  
+        this.compo.push(new DragAndDrop("h1", null, null, "mon super titre"));  
+        this.compo.push(new DragAndDrop("div", "section", "container"));  
+        this.compo.push(new DragAndDrop("button", null, "btn", "Button"));  
+        this.compo.push(new DragAndDrop("p", null, "text", "Hanc Atratino si cum qui ignosco acriter Atratino excusationem vel odio si meo est habet spei ignosco nihil optimo liceret hac excusationem habet libidine volueritis causa resistendum nec intolerabili cui si Si Ceteris nihil non necessitati de nisi spei nimis non existimare ego descensurum accusationem voluit excusationem spei meo descensurum volueritis vel ego vel ignosco causa intolerabili accusare optimo excusationem nec non causa speravit acerbo modo niteretur niteretur aetatis descendisset volueritis constituetis nimis fuisse necessario est alicuius sic descensurum necessario nec necessario iussus descensurum ignosco descendisset optimo aliquid iudices iussus modo si iudices Si necessitati est pietatis iudices quicquam etiam."));  
         // this.dom = dom.getDom(this.$refs.test,this.$refs.resultDom)
         // console.log(this.dom)
         // console.log(this.$refs.test)
         this.count = 0;
     }
 
-    startDrag(event: DragEvent, item: ComponentEdit){
+    startDrag(event: DragEvent, item: DragAndDrop){
         // event.dataTransfer.dropEffect = 'move';
         // event.dataTransfer.effectAllowed = 'move';
         this.element = item   
