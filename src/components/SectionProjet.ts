68944e68b4ff9818353e007388217ae2bd563fe4
nathan.eliezervanerot <nathan.eliezervanerot@eemi.com>
2021-03-03 11:22:41 +0100

    Add SectionProject

---
+++ src/components/SectionProjet.ts
@@ -0,0 +1,6 @@
+import { Component, Vue } from 'vue-property-decorator';
+import WithRender from '@/components/templates/SectionProjet.html'
+
+@WithRender
+@Component
+export default class SectionProjet extends Vue {}
--- src/views/Home.ts
+++ src/views/Home.ts
@@ -1,12 +1,14 @@
 import { Component, Vue } from 'vue-property-decorator';
 import WithRender from '@/views/templates/home.html'
 import MenuProjet from '@/components/MenuProjet'
+import SectionProjet from '@/components/SectionProjet'
 
 
 @WithRender
 @Component({
     components: {
-      MenuProjet
+      MenuProjet, 
+      SectionProjet
   }
 })
 export default class Home extends Vue {
